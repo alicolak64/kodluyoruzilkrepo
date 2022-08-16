66a0444ca304860845b7d6131c8771f8e0ffea00
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-16 15:44:54 +0300

    Added HarmonikOrtalama

---
+++ Java101/Diziler/HarmonikOrtalama.java
@@ -0,0 +1,14 @@
+package Java101.Diziler;
+
+public class HarmonikOrtalama {
+    public static void main(String[] args) {
+        double[] arr = { 1, 2, 3 };
+        double sum = 0;
+        for (double i : arr) {
+            sum += 1 / i;
+        }
+        double average = arr.length / sum;
+        System.out.println("Harmonik ortalama: " + average);
+
+    }
+}
