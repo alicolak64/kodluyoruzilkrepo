b2728d6172f112f266d2cb7163c80ecbf8db8e56
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-16 13:15:34 +0300

    Added VucutKitleIndeks

---
+++ Java101/Temel Kavramlar ve Degiskenler/VucutKitleIndeks.java
@@ -0,0 +1,17 @@
+public class {
+
+	public static void main(String[] args) {
+
+        int kilo;
+        double boy,sonuc;
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        boy = girdi.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kilo = girdi.nextInt();
+        sonuc = kilo/(boy*boy);
+        System.out.println("Vücut Kitle İndeksiniz : "+sonuc);
+        
+    }
+
+}
