52813f354ed70d2dfdb0b37b159c6b1077a766f3
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-08-26 18:50:59 +0300

    Added Toursim Agency Desktop App With Java

---
+++ Java102/TourismAgency/Helper/Config.java
@@ -0,0 +1,9 @@
+package Helper;
+
+public class Config {
+
+    public static final String PROJECT_TITLE = "Patika Turizm Acentesi";
+    public static final String DB_URL = "jdbc:mysql://localhost/hotel";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "******";
+}
